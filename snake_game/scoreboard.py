78dd990f242158b0f9b4e5c29fe521be5124f229
pranavisai <saipranavi1994@gmail.com>
2025-10-15 17:30:54 +0200

    retained high score using files.

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -8,6 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -15,11 +17,17 @@ class Scoreboard(Turtle):
         self.update_score()
 
     def update_score(self):
-        self.write(f"Score: {self.score}", align=ALIGN, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGN, font=FONT)
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", align=ALIGN, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(str(self.high_score))
+        self.score = 0
+        self.update_score()
+
 
     def increase_score(self):
         self.score += 1
--- turtle-crossing-app/main.py
+++ turtle-crossing-app/main.py
@@ -32,6 +32,7 @@ while game_is_on:
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.level_up()
+        score_board.increase_level()
 
 
 screen.exitonclick()
